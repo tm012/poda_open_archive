00ee0af1258f1a2f1f6f97c8a5d8d12609ca0995
Tauseef Mamun <tauseefim@yahoo.com>
2019-02-27 13:07:49 -0500

    make it better

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -12,7 +12,7 @@ class CreatePasswordResetsTable extends Migration
      * @return void
      */
     public function up()
-    {
+    {   Schema::defaultStringLength(191);
         Schema::create('password_resets', function (Blueprint $table) {
             $table->string('email')->index();
             $table->string('token');
